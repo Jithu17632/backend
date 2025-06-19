b0e68a7e063d37ff950fccb2791c806c3b403420
Jithu17632 <135446911+Jithu17632@users.noreply.github.com>
2025-06-19 20:10:01 +0530

    changes in deploye settings

--- exame_backend/exame_backend/deployment_settings.py
+++ exame_backend/exame_backend/deployment_settings.py
@@ -25,13 +25,13 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:8080",  
     # "http://127.0.0.1:8080",  
-    "https://karreo-sy-ss.onrender.com"
+    
     
 ]
 CSRF_TRUSTED_ORIGINS = [
     # "http://localhost:8080",  
     # "http://127.0.0.1:8080", 
-    "https://karreo-sy-ss.onrender.com"
+    
 ]
 
 STORAGES={
