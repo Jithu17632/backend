d6460ea78b7e70389832d6c25c287c0a05022f92
Jithu17632 <135446911+Jithu17632@users.noreply.github.com>
2025-06-19 19:46:35 +0530

    changes in buid.sh file again

--- exame_backend/build.sh
+++ exame_backend/build.sh
@@ -1,4 +1,4 @@
-set -e 
+set -o errexit
 
 pip install -r requirements.txt
 
@@ -6,6 +6,8 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if ["$CREATE_SUPERUSER"];then
-    python manage.py createsuperuser --no-input
+
+ if [[ $CREATE_SUPERUSER ]]
+ then
+     python manage.py createsuperuser --no-input
 fi
