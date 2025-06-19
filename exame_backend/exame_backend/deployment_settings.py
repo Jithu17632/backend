27eaab8e93850775838b375d6c735c6e380a8ffc
Jithu17632 <135446911+Jithu17632@users.noreply.github.com>
2025-06-19 20:06:43 +0530

    connected the front end

--- exame_backend/exame_backend/deployment_settings.py
+++ exame_backend/exame_backend/deployment_settings.py
@@ -22,10 +22,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]   
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8080",  
-#     "http://127.0.0.1:8080",  
-# ]
+CORS_ALLOWED_ORIGINS = [
+    # "http://localhost:8080",  
+    # "http://127.0.0.1:8080",  
+    "https://karreo-sy-ss.onrender.com"
+    
+]
+CSRF_TRUSTED_ORIGINS = [
+    # "http://localhost:8080",  
+    # "http://127.0.0.1:8080", 
+    "https://karreo-sy-ss.onrender.com"
+]
 
 STORAGES={
     "default":{
